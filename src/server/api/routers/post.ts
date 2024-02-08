862ad4c2b365eb11fd65624f3ccb434a8382f808
mhmadamrii <interceptorghost4@gmail.com>
2024-02-08 10:29:22 +0700

    db

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -15,7 +15,7 @@ export const postRouter = createTRPCRouter({
     .input(z.object({ name: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
       // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
+      // await new Promise((resolve) => setTimeout(resolve, 1000));
 
       return ctx.db.post.create({
         data: {
